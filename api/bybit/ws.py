b8009e2d951dd2e63dd54ea0bb280196c6587446
evgrmn <evgenyromensky@gmail.com>
2024-12-24 06:59:28 +0300

    Fixed Bybit orderbook callback

--- api/bybit/ws.py
+++ api/bybit/ws.py
@@ -194,14 +194,12 @@ class Bybit(Variables):
     def __update_orderbook(self, values: dict, category: str) -> None:
         symbol = (self.ticker[(values["s"], category)], self.name)
         instrument = self.Instrument[symbol]
-        values["a"].sort(key=lambda x: x[0])
-        values["b"].sort(key=lambda x: x[0], reverse=True)
-        asks = values["a"][:10]
-        bids = values["b"][:10]
-        asks = list(map(lambda x: [float(x[0]), float(x[1])], asks))
-        bids = list(map(lambda x: [float(x[0]), float(x[1])], bids))
-        instrument.asks = asks
-        instrument.bids = bids
+        asks = list(map(lambda x: [float(x[0]), float(x[1])], values["a"]))
+        bids = list(map(lambda x: [float(x[0]), float(x[1])], values["b"]))
+        asks.sort(key=lambda x: x[0])
+        bids.sort(key=lambda x: x[0], reverse=True)
+        instrument.asks = asks[:10]
+        instrument.bids = bids[:10]
         if symbol in self.klines:
             service.kline_hi_lo_values(self, symbol=symbol, instrument=instrument)
 
